ec9907701936bad8fea27415679d9175f9c53ecd
Aditya <adityakumar8340203184@gmail.com>
2025-04-08 18:31:12 +0530

    minor changes in cors

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 app.use(cookieParser())
